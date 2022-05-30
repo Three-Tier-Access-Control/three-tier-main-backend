c372a09c2a5b635819d3fbd53cc4495375cdce77
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-31 01:23:35 +0200

    custom

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
+    path('api/users/', include('users.urls')),
 
 
 ]
