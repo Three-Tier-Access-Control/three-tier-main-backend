0dfceb0dd60fdef77fac090276e9f03c1a9781cf
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-31 01:24:03 +0200

    custom

--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+from users.serializers import UserSerializer
+
 # Create your views here.
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
