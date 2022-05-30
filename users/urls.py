7f7fbfe1ab608d8751bf99daeeaa4ad0a830b7c9
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-31 01:25:19 +0200

    users

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from users.views import UserViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
