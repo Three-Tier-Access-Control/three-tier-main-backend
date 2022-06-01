dbc6f684e45521c2c23ac22eaf9eafcf511036ac
Shumba Ashley <ashleytshumba@gmail.com>
2022-06-01 02:03:55 +0200

    catch other exceprions

--- employees/views.py
+++ employees/views.py
@@ -26,20 +26,23 @@ class RFIDCardViewSet(viewsets.ModelViewSet):
             employee_id = data['employee']
             uid_tag = data['uid_tag']
             employee = Employee.objects.get(id=employee_id)
+
+            if RFIDCard.objects.filter(employee=employee):
+                return Response({"detail": f"Card for employee {employee_id} already exists "}, status=status.HTTP_400_BAD_REQUEST)
+
+            if RFIDCard.objects.filter(uid_tag=uid_tag):
+                return Response({"detail": f"Card id {uid_tag} already exists "}, status=status.HTTP_400_BAD_REQUEST)
+
+            rfid = RFIDCard.objects.create(employee=employee, uid_tag=uid_tag)
+
+            serializer = RFIDCardSerializer(rfid)
+            return Response(serializer.data)
+
         except Employee.DoesNotExist:
             return Response({"detail": f"Employee {employee_id} does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-        if RFIDCard.objects.filter(employee=employee):
-            return Response({"detail": f"Card for employee {employee_id} already exists "}, status=status.HTTP_400_BAD_REQUEST)
-
-        if RFIDCard.objects.filter(uid_tag=uid_tag):
-            return Response({"detail": f"Card id {uid_tag} already exists "}, status=status.HTTP_400_BAD_REQUEST)
-
-
-        rfid = RFIDCard.objects.create(employee=employee, uid_tag=uid_tag)
-
-        serializer = RFIDCardSerializer(rfid)
-        return Response(serializer.data)
+        except Exception as e:
+            return Response({"detail": f"Error:  {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class FingerPrintViewSet(viewsets.ModelViewSet):
@@ -53,17 +56,21 @@ class FingerPrintViewSet(viewsets.ModelViewSet):
             fingerprint_id = data['fingerprint_id']
 
             employee = Employee.objects.get(id=employee_id)
+
+            if FingerPrint.objects.filter(employee=employee):
+                return Response({"detail": f"Fingerprint for employee {employee_id} already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+            if FingerPrint.objects.filter(fingerprint_id=fingerprint_id):
+                return Response({"detail": f"Fingerprint id {fingerprint_id} already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+            fingerprint = FingerPrint.objects.create(employee=employee,
+                                                     fingerprint_id=fingerprint_id)
+
+            serializer = FingerPrintSerializer(fingerprint)
+            return Response(serializer.data)
+
         except Employee.DoesNotExist:
             return Response({"detail": f"Employee {employee_id} does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
-        if FingerPrint.objects.filter(employee=employee):
-            return Response({"detail": f"Fingerprint for employee {employee_id} already exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        if FingerPrint.objects.filter(fingerprint_id=fingerprint_id):
-            return Response({"detail": f"Fingerprint id {fingerprint_id} already exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        fingerprint = FingerPrint.objects.create(employee=employee,
-                                                 fingerprint_id=fingerprint_id)
-
-        serializer = FingerPrintSerializer(fingerprint)
-        return Response(serializer.data)
+        except Exception as e:
+            return Response({"detail": f"Error:  {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
