f5d2c62431c0165e3ef9f81cdf8848794fb4828c
Shumba Ashley <ashleytshumba@gmail.com>
2022-06-06 21:56:51 +0200

    Access Log  with Status

--- access/admin.py
+++ access/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from access.models import AccessLog
+
 # Register your models here.
+@admin.register(AccessLog)
+class AccessLogAdmin(admin.ModelAdmin):
+    list_display = ("employee", "direction", "created")
+    list_filter = ("employee", "direction",)
+    date_hierarchy = 'created'
--- access/models.py
+++ access/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from employees.models import Employee
 from utils.models import TimeStampedUUIDModel
 
+
 class AccessLog(TimeStampedUUIDModel):
     IN = "in"
-    OUT="out"
+    OUT = "out"
     DIRECTION_CHOICES = [(IN, "IN"), (OUT, "OUT"), ]
     employee = models.OneToOneField(
         Employee, on_delete=models.CASCADE, related_name="access_logs")
-    direction = models.CharField(max_length=254, choices=DIRECTION_CHOICES, default=IN)
+    direction = models.CharField(
+        max_length=254, choices=DIRECTION_CHOICES, default=IN)
+    status = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = "Access Log"
@@ -16,4 +19,4 @@ class AccessLog(TimeStampedUUIDModel):
         ordering = ('-created',)
 
     def __str__(self):
-        return self.employee
+        return f"{self.employee}"
