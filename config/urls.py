44da0118e6cf84d074e89c4ebd308752d2304ef6
Shumba Ashley <ashleytshumba@gmail.com>
2022-06-22 01:21:56 +0200

    rest api  backend

--- config/urls.py
+++ config/urls.py
@@ -26,11 +26,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Three Tier Main Backend API",
+        title="Three Tier Main Backend REST API",
         default_version='v1',
-        description="Test description",
+        description="Three Tier Security Main Backend",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="ashleytshumba@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -43,9 +43,9 @@ router.register(r'fingerprint', FingerPrintViewSet)
 
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+    re_path(r'^(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+    re_path(r'^$', schema_view.with_ui('swagger',
                                                cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc',
                                              cache_timeout=0), name='schema-redoc'),
